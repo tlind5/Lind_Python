e8e022ebb628a30484934f337bf30ca6f455bdca
tlind5 <trentlind8@gmail.com>
2022-04-08 14:17:55 -0500

    Loading Bar 2

--- testCSVWrite Script.py
+++ testCSVWrite Script.py
@@ -3,6 +3,21 @@ from random import randint
 import time
 import numpy as np
 
+import sys
+
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
+
 x = []
 y = []
 x=1
@@ -51,10 +66,17 @@ while b <= a:
     b = b+1
     P = P + q
 
-    loadingBar(a,b)
+    #loadingBar(a,b)
+
+
 
     with open(r'C:\Users\WKYTUK0\OneDrive-Deere&Co\OneDrive - Deere & Co\Documents\Internship2021\ExcelDocuments\UpdateCSV.csv', mode='a', newline='') as csvFile:
         file_writer = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file_writer.writerow([x,y])
 
-    time.sleep(1)
+    #time.sleep(1)
+
+import time
+
+for i in progressbar(range(15), "Computing: ", 40):
+    time.sleep(1) # any calculation you need
