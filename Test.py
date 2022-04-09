fe8f4a33cfc1905e5e2a492fea947050820f4a0a
tlind5 <trentlind8@gmail.com>
2022-04-09 10:11:03 -0500

    Loading Bar GUI

--- Test.py
+++ Test.py
@@ -1,1 +1,23 @@
-print("ORd Rentner")
+from tkinter import *
+from tkinter.ttk import *
+import time
+
+ws = Tk()
+ws.title('Progress Bar')
+ws.geometry('400x250+1000+300')
+
+
+def step():
+    for i in range(50):
+        ws.update_idletasks()
+        pb1['value'] += 2
+
+        time.sleep(.2)
+
+
+pb1 = Progressbar(ws, orient=HORIZONTAL, length=100, mode='determinate')
+pb1.pack(expand=True)
+
+Button(ws, text='Start', command=step).pack()
+
+ws.mainloop()
