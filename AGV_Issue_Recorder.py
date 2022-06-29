b14334d3fc66aaed5297744bbe7efd44a4d65c3d
tlind5 <trentlind8@gmail.com>
2022-06-29 13:35:30 -0500

    Excel To CSV

--- AGV_Issue_Recorder.py
+++ AGV_Issue_Recorder.py
@@ -2,9 +2,14 @@ import csv
 
 def openCSV():
     try:
-        with open(r'C:\Users\WKYTUK0\Documents\TestingCode\PumpData.csv', mode='a', newline='') as df:
+        with open(r'C:\Users\WKYTUK0\OneDrive-Deere&Co\OneDrive - Deere & Co\Documents\EDP Rotation 1\Excel Documents\Data Analytics\AGV Data.csv', mode='a', newline='') as df:
             file_writer = csv.writer(df, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     except IOError:
+        with open(r'C:\Users\WKYTUK0\OneDrive-Deere&Co\OneDrive - Deere & Co\Documents\EDP Rotation 1\Excel Documents\Data Analytics\AGV Data.csv', mode='w+', newline='') as df:
+            file_writer = csv.writer(df, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            file_writer.writerow("Alpha")
+            file_writer.writerow("Beta")
+
         print('The file could not be opened')
 
 
